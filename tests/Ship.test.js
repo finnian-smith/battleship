985f5d6596a0fd3ebda3463c92e44904ceab6491
finnian-smith <finniansmith01@gmail.com>
2024-07-09 14:42:09 +0100

    Create Ship class and testing

---
+++ tests/Ship.test.js
@@ -0,0 +1,26 @@
+const Ship = require("../src/Ship");
+
+test("Ship created with correct length", () => {
+  const ship = new Ship(3);
+  expect(ship.length).toBe(3);
+});
+
+test("hit() increases the hit count", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
+
+test("isSunk() returns true when hits >= length", () => {
+  const ship = new Ship(3);
+  for (let i = 0; i < ship.length; i++) {
+    ship.hit();
+  }
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("isSunk() returns false when hits < length", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
