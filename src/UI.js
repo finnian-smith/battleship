76a8f522bf674296f5e15b87a9d15b0e20a50204
finnian-smith <finniansmith01@gmail.com>
2024-07-10 09:34:41 +0100

    Create UI module

---
+++ src/UI.js
@@ -0,0 +1,28 @@
+// implement ui module
+import Game from "./Game";
+
+function renderBoard(board, element) {
+  element.innerHTML = "";
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+
+      if (
+        board.some((item) =>
+          item.coordinates.some((coord) => coord[0] === i && coord[1] === j)
+        )
+      ) {
+        cell.classList.add("ship");
+      }
+      element.appendChild(cell);
+    }
+  }
+}
+
+function renderGame(game, player1Board, player2Board) {
+  renderBoard(game.player1.gameboard.board, player1Board);
+  renderBoard(game.player2.gameboard.board, player2Board);
+}
+
+export default { renderBoard, renderGame };
